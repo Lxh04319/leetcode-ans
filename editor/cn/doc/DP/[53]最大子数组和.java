1b07632a3e38a4a2d4bea6cddad9e669b9567d1d
lxh11111 <lxh3162335158@qq.com>
2024-03-18 20:01:40 +0800

    动态规划

---
+++ editor/cn/doc/DP/[53]最大子数组和.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int[] dp=new int[nums.length];
+        dp[0]=nums[0];
+        int maxx=dp[0];
+        for(int i=1;i<nums.length;i++){
+            dp[i]=Math.max(dp[i-1]+nums[i],nums[i]);
+            maxx=Math.max(maxx,dp[i]);
+        }
+        return maxx;
+    }
+}
