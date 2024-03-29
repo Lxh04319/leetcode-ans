4944ac72f4fa56e74f89a57f859bbdae318366b6
lxh11111 <lxh3162335158@qq.com>
2024-03-29 22:40:31 +0800

    hot100

---
+++ editor/cn/hot100/二叉树/[543]二叉树的直径.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int res=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    public int dfs(TreeNode root){
+        if(root==null) return 0;
+        int left=dfs(root.left);
+        int right=dfs(root.right);
+        res=Math.max(res,left+right);
+        return Math.max(left,right)+1;
+    }
+
+}
