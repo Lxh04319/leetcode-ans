814035f565a940bd559f24aa4ca9819930a00517
lxh11111 <lxh3162335158@qq.com>
2024-03-31 21:48:06 +0800

    hot100

--- editor/cn/hot100/二分查找/[74]搜索二维矩阵.java
+++ editor/cn/hot100/二分查找/[74]搜索二维矩阵.java
@@ -1,3 +1,18 @@
+//二分查找
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length,n=matrix[0].length;
+        int left=0,right=m*n-1;
+        while(left<=right){
+            int mid=(right-left)/2+left;
+            if(matrix[mid/n][mid%n]==target) return true;
+            if(matrix[mid/n][mid%n]<target) left=mid+1;
+            else right=mid-1;
+        }
+        return false;
+    }
+}
+//抽象bst
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         //bst 
